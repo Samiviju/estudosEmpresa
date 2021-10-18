37952a56363aca4e3ec5de8c479e1cf60be272fd
Samuel Vieira <goodluff@hotmail.com>
2021-10-17 22:21:24 -0300

    Adicionado um Console.WriteLine!

---
+++ estudosEmpresa/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace estudosEmpresa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um valor: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite outro valor");
+            int b = int.Parse(Console.ReadLine());
+
+            if (a > b)
+            {
+                Console.WriteLine("Exato, está correto");
+            }
+
+            else if (a == b)
+            {
+                Console.WriteLine("Parabéns! Eles são iguais!");
+            }
+
+            else
+            {
+                Console.WriteLine("Errado");
+            }
+
+            Console.WriteLine("Alo");
+
+
+
+
+
+
+        }
+    }
+}
